21d49574a7ca5051d6c2c2ac0d479c0473cf6d12
Wisnu Mulya <wisnu@wisnumulya.com>
2015-10-08 20:53:37 +0700

    add: Saving data to localStorage and some stylings

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,65 @@
 'use strict';
 
+// Run function when page loaded to initialize app
 $(function() {
+  if (!localStorage.locations || (JSON.parse(localStorage.locations).length < 15)) {
+    createLocalStorage();
+    setTimeout(init, 1000); // Wait for the localStorage to be built
+  } else {
+    init();
+  }
+});
+
+// Create local storage data when there's none or when the data is not complete
+function createLocalStorage() {
+  localStorage.locations = '[]'; // Initialize localStorage array
+  var localStorageObject;
+
+  var places = [
+    {"name": "Museum Satria Mandala", "type": "landmark"},
+    {"name": "Taman Suropati", "type": "park"},
+    {"name": "Taman Menteng", "type": "park"},
+    {"name": "Gelora Bung Karno", "type": "landmark"},
+    {"name": "Monumen Nasional", "type": "landmark"},
+    {"name": "Kebun Binatang Ragunan", "type": "zoo"},
+    {"name": "Taman Mini Indonesia Indah", "type": "theme-park"},
+    {"name": "Kuningan City Mall", "type": "mall"},
+    {"name": "Mall of Indonesia", "type": "mall"},
+    {"name": "Pondok Indah Mall", "type": "mall"},
+    {"name": "Plaza Senayan", "type": "mall"},
+    {"name": "Plaza Indonesia Mall", "type": "mall"},
+    {"name": "Grand Indonesia Mall", "type": "mall"},
+    {"name": "Dunia Fantasi", "type": "theme-park"},
+    {"name": "Taman Safari", "type": "zoo"}
+  ];
+
+  // Building local storage by using Google Geocoding API for requesting location's lat & lng
+  places.forEach(function(place) {
+    $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC4SBS-k9EzqVIQS67qjljL-Q5wlzftYY4&address=' + place.name,
+      function(data) {
+        place.position = data.results[0].geometry.location;
+        localStorageObject = JSON.parse(localStorage.locations);
+        localStorageObject.push(place);
+        localStorage.locations = JSON.stringify(localStorageObject);
+    }).error(function() {
+      console.log('error');
+    });
+  });
+};
+
+
+// Initialize app functionalities
+function init() {
   // Variable to center the position of the map at initialization
   var jakarta = {name: "jakarta", position: {lat: -6.2087634, lng: 106.845599}};
 
   // Spots to be shown in the view
-  var jakartaSpots = [
-    {name: 'Museum Satria Mandala', position: {lat: -6.231082, lng: 106.819013}, type: 'landmark'},
-    {name: 'Taman Suropati', position: {lat: -6.1993382, lng: 106.8324018}, type: 'landmark'},
-    {name: 'Gelora Bung Karno', position: {lat: -6.218597, lng: 106.8017673}, type: 'landmark'},
-    {name: 'Monumen Nasional', position: {lat: -6.1753924, lng: 106.8271528}, type: 'landmark'},
-    {name: 'Kebun Binatang Ragunan', position: {lat: -6.3124593, lng: 106.8201865}, type: 'zoo'},
-    {name: 'Taman Menteng', position: {lat: -6.196162999999999, lng: 106.829615}, type: 'landmark'},
-    {name: 'Taman Mini Indonesia Indah', position: {lat: -6.302445899999999, lng: 106.8951559}, type: 'theme-park'},
-    {name: "Pondok Indah Mall", position: {lat: -6.266409299999999, lng: 106.7836598}, type: 'mall'},
-    {name: "Plaza Senayan", position: {lat: -6.2255809, lng: 106.7999552}, type: 'mall'}
-  ];
+  var jakartaSpots = JSON.parse(localStorage.locations);
 
   // Initializing Google Maps
   var map = new google.maps.Map(document.getElementById('map'), {
     center: jakarta.position,
-    zoom: 12
+    zoom: 10
   });
 
   // Initializing infowindow to be shown above the map markers
@@ -33,6 +72,8 @@ $(function() {
     map.panTo(marker.getPosition())
     infowindow.open(map, marker);
     infowindow.setContent('Loading...');
+
+    // Requesting images from Flickr
     $.getJSON('https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
       format: 'json',
       tags: locName
@@ -108,4 +149,4 @@ $(function() {
   };
 
   ko.applyBindings(new viewModel());
-});
+}
